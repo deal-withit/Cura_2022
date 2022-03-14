dad8caf355a283b5ca2e46ec5fc369845086b4f2
stressedtyagi <divyanshutyagi8@gmail.com>
2022-03-14 10:20:14 +0530

    test commit

--- src/App.js
+++ src/App.js
@@ -2,34 +2,33 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar, Footer, ScrollToTop } from "./components";
 import GlobalStyle from "./globalStyles";
-import ComingSoon from "./ComingSoon"
+import ComingSoon from "./ComingSoon";
 import MainPage from "./components/MainPage";
-import Competitions from './components/Competitions'
-import Speakers from './components/Speakers'
-import Workshops from './components/Workshops'
-import Teams from './components/Teams'
-import Testimonials from './components/Testimonials'
-import Partners from './components/Partners'
+import Competitions from "./components/Competitions";
+import Speakers from "./components/Speakers";
+import Workshops from "./components/Workshops";
+import Teams from "./components/Teams";
+import Testimonials from "./components/Testimonials";
+import Partners from "./components/Partners";
 function App() {
-  return (
-    <Router> 
-      <GlobalStyle />
-      <ScrollToTop />
-      <Navbar />
-      <Routes>
-        <Route path="/" exact element={<MainPage/>}/>
-        <Route path="/comingsoon" element={<ComingSoon/>}/>
-        <Route path="/speakers" element={<Speakers/>}/>
-        <Route path="/workshops" element={<Workshops/>}/>
-        <Route path="/competitions" element={<Competitions/>}/>
-        <Route path="/partners" element={<Partners/>}/>
-        <Route path="/teams" element={<Teams/>}/>
-        <Route path="/Testimonials" element={<Testimonials/>}/>
-      </Routes>
-      <Footer/>
-      {/* <Foot/> */}
-    </Router>
-  );
+    return (
+        <Router>
+            <GlobalStyle />
+            <ScrollToTop />
+            <Navbar />
+            <Routes>
+                <Route path="/" exact element={<MainPage />} />
+                <Route path="/comingsoon" element={<ComingSoon />} />
+                <Route path="/speakers" element={<Speakers />} />
+                <Route path="/workshops" element={<Workshops />} />
+                <Route path="/competitions" element={<Competitions />} />
+                <Route path="/partners" element={<Partners />} />
+                <Route path="/teams" element={<Teams />} />
+                <Route path="/Testimonials" element={<Testimonials />} />
+            </Routes>
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
